1af592fd1b29ce23a5efbe2e41581125fff2031c
Victor <victorbucar@outlook.com>
2019-04-19 20:07:13 +0100

    Added home and register component

---
+++ SchoolConnection-SPA/src/app/register/register.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() valuesFromHome: any // to receive from a parent component
+  @Output() cancelRegister = new EventEmitter()
+  registerModel: any = {}
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    console.log(this.registerModel)
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false)
+    console.log('cancelled')
+  }
+}
