dd7d9cafd863d77c2596af4c1bc686728bcead4f
Victor <victorbucar@outlook.com>
2019-04-19 21:23:49 +0100

    register method in angular

--- SchoolConnection-SPA/src/app/register/register.component.ts
+++ SchoolConnection-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,18 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any // to receive from a parent component
   @Output() cancelRegister = new EventEmitter()
   registerModel: any = {}
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
     console.log(this.registerModel)
+    this.authService.register(this.registerModel).subscribe(() => {
+      console.log('registered')
+    }, error => {
+      console.log(error)
+    })
   }
 
   cancel() {
